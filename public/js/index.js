0d142f0d87dfd1d8fe80a54303c2ce9fa465728b
spurgason <shanepurgason.98@gmail.com>
2021-12-04 10:54:29 -0500

    pasted url in post and fetch

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 let transactions = [];
 let myChart;
 
-fetch("/api/transaction")
+fetch("https://fast-spire-15067.herokuapp.com/api/transaction")
   .then(response => {
     return response.json();
   })
@@ -113,7 +113,7 @@ function sendTransaction(isAdding) {
   populateTotal();
   
   // also send to server
-  fetch("/api/transaction", {
+  fetch("https://fast-spire-15067.herokuapp.com/api/transaction", {
     method: "POST",
     body: JSON.stringify(transaction),
     headers: {
